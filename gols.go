dd85373240e9c2ee0f94fd96f99f6a54d1af8622
Francis Nimick <fnimick@gmail.com>
2016-03-23 23:02:57 -0400

    Directory reading and output, no link support yet

--- gols.go
+++ gols.go
@@ -2,8 +2,83 @@ package main
 
 import (
   "github.com/docopt/docopt-go"
+  "gopkg.in/yaml.v2"
+  "encoding/json"
   "fmt"
+  "io/ioutil"
+  "os"
+  "path/filepath"
+  "time"
 )
+
+type NestedFile struct {
+  ModifiedTime time.Time
+  IsLink bool
+  IsDir bool
+  LinksTo string
+  Size int64
+  Name string
+  Children []NestedFile
+}
+
+func getLinkInfo(f os.FileInfo) string {
+  // TODO
+  return ""
+}
+
+func toNestedFile(f os.FileInfo, children []NestedFile) NestedFile {
+  return NestedFile{
+    f.ModTime(),
+    false,
+    f.IsDir(),
+    getLinkInfo(f),
+    f.Size(),
+    f.Name(),
+    children,
+  }
+}
+
+func dirReader(dir string, recursive bool) []NestedFile {
+  files, _ := ioutil.ReadDir(dir)
+  output := make([]NestedFile, 0, len(files))
+  for _, f := range files {
+    children := []NestedFile{}
+    if f.IsDir() && recursive {
+      children = dirReader(filepath.Join(dir, f.Name()), recursive)
+    }
+    output = append(output, toNestedFile(f, children))
+  }
+  return output
+}
+
+func textOutputHelper(files []NestedFile, out *os.File, indent string) {
+  for _, f := range files {
+    fmt.Fprint(out, indent + f.Name)
+    if f.IsDir {
+      fmt.Fprintln(out, "/")
+      textOutputHelper(f.Children, out, indent + "  ")
+    } else if f.IsLink {
+      fmt.Fprintln(out, "* (" + f.LinksTo + ")")
+    } else {
+      fmt.Fprintln(out, "")
+    }
+  }
+}
+
+func textOutput(files []NestedFile, out *os.File) {
+  textOutputHelper(files, out, "")
+}
+
+func jsonOutput(files []NestedFile, out *os.File) {
+  outJson, _ := json.MarshalIndent(files, "", "  ")
+  fmt.Fprintln(out, string(outJson))
+}
+
+func yamlOutput(files []NestedFile, out *os.File) {
+  outYaml, _ := yaml.Marshal(files)
+  fmt.Fprintln(out, string(outYaml))
+}
+
 
 func main() {
   usage := `gols.
@@ -22,5 +97,22 @@ Options:
     panic(err)
   }
 
-  fmt.Println(arguments)
+  format := arguments["--output"].(string)
+
+  outputFormatters := map[string]func([]NestedFile, *os.File) {
+    "json": jsonOutput,
+    "text": textOutput,
+    "yaml": yamlOutput,
+  }
+
+  formatter, ok := outputFormatters[format]
+  if !ok {
+    fmt.Println("Invalid output format provided.")
+    os.Exit(1)
+  }
+
+  path := arguments["--path"].(string)
+  absPath, _ := filepath.Abs(path)
+  recursive := arguments["--recursive"].(bool)
+  formatter(dirReader(absPath, recursive), os.Stdout)
 }
